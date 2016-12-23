65adf8abc66638c7846640c0e67179e67b62b9c8
ZYXLuffY <zhouxiangluffy@163.com>
2016-12-23 16:48:48 +0800

    Initial Commit

---
+++ XZ_Demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XZ_Demo
+//
+//  Created by iOS_XZ on 2016/12/23.
+//  Copyright © 2016年 iOS_XZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XZ_Demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XZ_Demo
+//
+//  Created by iOS_XZ on 2016/12/23.
+//  Copyright © 2016年 iOS_XZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
